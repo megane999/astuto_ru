c4266864d4abe61397256e7c586b8c2ff02ba31e
megane999 <megane999@gmail.com>
2022-09-01 16:22:58 +0300

    Update seeds.rb
    Перевод на русский

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Create tenant
 tenant = Tenant.create(
-  site_name: 'Default site name',
+  site_name: 'Идеи для STATTECH',
   subdomain: 'default',
   status: 'active'
 )
@@ -17,37 +17,37 @@ admin = User.create(
 
 # Create some boards
 feature_board = Board.create(
-  name: 'Feature Requests',
+  name: 'Новые идеи',
   description: 'This is a **board**! You can create as many as you want from **site settings** and their description can be *Markdown formatted*.',
   order: 0
 )
 bug_board = Board.create(
-  name: 'Bug Reports',
+  name: 'Отчеты об ошибках',
   description: 'Tell us everything about problems you encountered in our services!',
   order: 1
 )
 
 # Create some post statuses
 planned_post_status = PostStatus.create(
-  name: 'Planned',
+  name: 'Запланировано',
   color: '#0096ff',
   order: 0,
   show_in_roadmap: true
 )
 in_progress_post_status = PostStatus.create(
-  name: 'In Progress',
+  name: 'В прогрессе',
   color: '#9437ff',
   order: 1,
   show_in_roadmap: true
 )
 completed_post_status = PostStatus.create(
-  name: 'Completed',
+  name: 'Завершено',
   color: '#6ac47c',
   order: 2,
   show_in_roadmap: true
 )
 rejected_post_status = PostStatus.create(
-  name: 'Rejected',
+  name: 'Отклонено',
   color: '#ff2600',
   order: 3,
   show_in_roadmap: false
@@ -55,7 +55,7 @@ rejected_post_status = PostStatus.create(
 
 # Create some posts
 post1 = Post.create(
-  title: 'Users can submit feedback by publishing posts!',
+  title: 'Пользователи могут оставлять отзывы, публикуя сообщения!',
   description: 'You can assign a **status** to each post: this one, for example, is marked as "Planned". Remember that you can customise post statuses from Site settings > Statuses',
   board_id: feature_board.id,
   user_id: admin.id,
@@ -68,7 +68,7 @@ PostStatusChange.create(
 )
 
 post2 = Post.create(
-  title: 'There are multiple boards',
+  title: 'Есть несколько досок',
   description: 'For now you have Feature Requests and Bug Reports, but you can add or remove as many as you want! Just go to Site settings > Boards!',
   board_id: bug_board.id,
   user_id: admin.id
